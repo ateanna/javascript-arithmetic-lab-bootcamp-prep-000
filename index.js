28796017957c500661d184df94b7855af293002d
Maria Anna San Luis <maansanluis@gmail.com>
2018-11-02 02:57:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,8 +16,8 @@ function inc(n){
 function dec(n){
   return n-1
 }
-function makeInt(n){
-  return parseInt(n)
+function makeInt(n, base){
+  return parseInt(n, base)
 }
 function preserveDecimal(n){
   return parseFloat(n)
